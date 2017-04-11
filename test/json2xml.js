cd594ccaee60b7f45a18b03b757aff53e7a97d14
glenna-m <glenna.hokukahu@gmail.com>
2017-04-10 18:00:04 -0700

    Moved to node 7.8 + added tests & some cleanup - initial goal was to ensure this code works on node 7.8 - Found there was no working automated testing, so wrote some - Modified test files to use cmumps and 01/01/1990 dates - Documented manual test tool

--- test/json2xml.js
+++ test/json2xml.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 /**
+ * A manual test tool for this package by
  * Mike Carifio <mike@carif.io> (https://mike.carif.io/)
+ *
+ * Syntax: 
+ *   json2xml.js <a json file to test>
+ * Example 
+ *   ./json2xml.js demographic.json
  */
 
-
+var fs = require('fs');
 var process = require('process');
 var program = require('commander');
+
+var index = __dirname + '/../index.js';
+var fhir2xml = require(index); // hacked version of fhir-convert.js that exports certain functions
+
 program.parse(process.argv);
-
-var fhir2xml = require('../index.js'); // hacked version of fhir-convert.js that exports certain functions
-var fs = require('fs');
-
 program.args.forEach(function(pathname) {
     var json = JSON.parse(fs.readFileSync(pathname, "utf8"));
     var parser = new fhir2xml.FHIRConverter(2);
